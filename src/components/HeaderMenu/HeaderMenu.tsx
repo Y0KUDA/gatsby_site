e9d1df0071639b9365fb908f14e4d302ce2e0f00
Y0KUDA <40825115+Y0KUDA@users.noreply.github.com>
2019-07-18 01:38:07 +0900

    fix layout ; add menu icon and blog title

--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { toggleSidebar } from "../../store";
-import { Container, Label, Menu, Icon } from "semantic-ui-react";
+import { Container, Label, Menu, Icon, Header } from "semantic-ui-react";
 import { MenuProps } from "../Menu";
 
 interface HeaderMenuProps extends MenuProps {
@@ -12,14 +12,21 @@ interface HeaderMenuProps extends MenuProps {
 
 export const HeaderMenu = ({ items, pathname, Link, inverted, dispatch }: HeaderMenuProps) =>
   <Container>
+    <Link to="/">
+      <Header as="h1" style = {{ margin: "1rem 0 1rem 0" }}>
+        <Icon name="rocket" />
+        <Header.Content>My Awesome Blog</Header.Content>
+        <Header.Subheader>　　　My super hyper goddamn awesome special cool site.</Header.Subheader>
+      </Header>
+    </Link>
     <Menu size="large" pointing secondary inverted={inverted}>
-      <Menu.Item as="a" className="mobile only" icon="sidebar" onClick={() => dispatch && dispatch(toggleSidebar())} />
-      <Menu.Item className="mobile hidden"><Icon name="spy" size="big" /></Menu.Item>
+      <Menu.Item as="a" className="mobile only" icon="sidebar"  onClick={() => dispatch && dispatch(toggleSidebar())} />
       {items.map((item) => {
         const active = (item.exact) ? pathname === item.path : pathname.startsWith(item.path);
         return <Menu.Item
           as={Link}
           className="mobile hidden"
+          icon={item.icon}
           name={item.name}
           to={item.path}
           key={item.path}
