70ffaea36c3eee6259ea1ac7b93266b71177a723
Y0KUDA <40825115+Y0KUDA@users.noreply.github.com>
2019-07-19 23:18:06 +0900

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+        trackingId: 'UA-144161806-1',
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
